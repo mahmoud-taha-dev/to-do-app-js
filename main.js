31c39673a19ccb293a432f1a8a6b279c951d4f25
Mahmoud Taha <mahmoud.taha.dev@gmail.com>
2021-07-30 07:28:59 +0200

    add edit and delete buttons

--- main.js
+++ main.js
@@ -1,31 +1,70 @@
-var tasksString = localStorage.getItem("tasks");
-var tasks = tasksString ? JSON.parse(tasksString) : [];
-var input = document.querySelector("input");
-var containers = document.querySelectorAll(".status-cont");
+const tasksString = localStorage.getItem("tasks");
+let tasks = tasksString ? JSON.parse(tasksString) : [];
+const input = document.querySelector("input");
+const containers = document.querySelectorAll(".status-cont");
+const addBtn = document.getElementById("addBtn");
+let btnAction = "add";
+let modifiedTask;
 
 const addToContainer = (text, id, contId) => {
-  var item = document.createElement("p");
-  document.getElementById(contId).appendChild(item);
-  item.className = "task";
-  item.textContent = text;
-  item.id = id;
-  item.setAttribute("draggable", "true");
-  item.addEventListener("dragstart", (e) => {
+  const taskCont = document.createElement("div");
+  document.getElementById(contId).appendChild(taskCont);
+  taskCont.className = "task";
+  taskCont.id = id;
+  taskCont.setAttribute("draggable", "true");
+  taskCont.addEventListener("dragstart", (e) => {
     e.dataTransfer.setData("text", id);
+  });
+  const taskTxt = document.createElement("p");
+  taskCont.appendChild(taskTxt);
+  taskTxt.textContent = text;
+  const icons = document.createElement("div");
+  taskCont.appendChild(icons);
+  const editIcon = document.createElement("i");
+  icons.appendChild(editIcon);
+  editIcon.className = "fas fa-edit";
+  editIcon.addEventListener("click", () => {
+    handleEditIcon(text, id);
+  });
+  const delIcon = document.createElement("i");
+  icons.appendChild(delIcon);
+  delIcon.className = "fas fa-trash";
+  delIcon.addEventListener("click", () => {
+    handleDeleteIcon(id);
   });
 };
 
 const addTask = (text) => {
-  var uniqId =
+  const uniqId =
     Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
   tasks.push({
-    taskId: uniqId,
-    taskName: text,
+    id: uniqId,
+    name: text,
     status: "inProgress",
   });
   localStorage.setItem("tasks", JSON.stringify(tasks));
   addToContainer(text, uniqId, "inProgress");
-  input.value = "";
+};
+
+const updateTask = () => {
+  tasks = tasks.map((task) => {
+    return task.id === modifiedTask ? { ...task, name: input.value } : task;
+  });
+  document.querySelector(`#${modifiedTask} > p`).textContent = input.value;
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const handleEditIcon = (text, id) => {
+  input.value = text;
+  addBtn.textContent = "Update";
+  btnAction = "update";
+  modifiedTask = id;
+};
+
+const handleDeleteIcon = (id) => {
+  tasks = tasks.filter((task) => task.id !== id);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  document.getElementById(id).remove();
 };
 
 const dragoverFunction = (e) => {
@@ -33,10 +72,10 @@ const dragoverFunction = (e) => {
 };
 
 const dropFunction = (e) => {
-  var containerId = e.target.id;
-  var dataId = e.dataTransfer.getData("text");
+  const containerId = e.target.id;
+  const dataId = e.dataTransfer.getData("text");
   document.getElementById(containerId).append(document.getElementById(dataId));
-  const index = tasks.findIndex((item) => item.taskId === dataId);
+  const index = tasks.findIndex((item) => item.id === dataId);
   tasks[index].status = containerId;
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
@@ -47,17 +86,21 @@ const deleteTasks = () => {
 };
 
 tasks.length &&
-  tasks.forEach((task) =>
-    addToContainer(task.taskName, task.taskId, task.status)
-  );
+  tasks.forEach((task) => addToContainer(task.name, task.id, task.status));
 
 containers.forEach((item) => {
   item.addEventListener("dragover", dragoverFunction);
   item.addEventListener("drop", dropFunction);
 });
 
-document.getElementById("addBtn").addEventListener("click", () => {
-  input.value && addTask(input.value);
-});
+const handleAddBtn = () => {
+  input.value && btnAction === "add" && addTask(input.value);
+  input.value && btnAction === "update" && updateTask(input.value);
+  btnAction = "add";
+  addBtn.textContent = "Add";
+  input.value = "";
+};
+
+addBtn.addEventListener("click", handleAddBtn);
 
 document.getElementById("clearBtn").addEventListener("click", deleteTasks);
